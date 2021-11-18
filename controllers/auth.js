276012e83c9bcef465eef7649cf29b4c0080fb09
Robert  Saul <saulr18@students.ecu.edu>
2021-11-18 15:19:48 -0500

    feat: Add login route with password check

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const {validationResult} = require("express-validator/check");
+const { validationResult } = require("express-validator/check");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
@@ -26,9 +26,39 @@ exports.signup = (req, res, body) => {
     .then(result => {
       res.status(201).json({ message: "User created", userId: result.id });
     }).catch(err => {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  });
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+}
+
+exports.login = (req, res, body) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Login failed.");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ where: { email: email } })
+    .then(user => {
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            return res.status(201).json({ message: "Correct password." })
+          }
+          return res.status(422).json({ message: "Incorrect password." })
+        })
+        .catch(err => {
+          if (!err.statusCode) {
+            err.statusCode = 500;
+          }
+          next(err);
+        })
+    })
+
 }
