AlexsLaboratory/notes-api

commit 276012e83c9bcef465eef7649cf29b4c0080fb09

feat: Add login route with password check

controllers/auth.js
@@ -1,4 +1,4 @@
-const {validationResult} = require("express-validator/check");
+const { validationResult } = require("express-validator/check");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
@@ -26,9 +26,39 @@ exports.signup = (req, res, body) => {
     .then(result => {
       res.status(201).json({ message: "User created", userId: result.id });
     }).catch(err => {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  });
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+}
+
+exports.login = (req, res, body) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Login failed.");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ where: { email: email } })
+    .then(user => {
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            return res.status(201).json({ message: "Correct password." })
+          }
+          return res.status(422).json({ message: "Incorrect password." })
+        })
+        .catch(err => {
+          if (!err.statusCode) {
+            err.statusCode = 500;
+          }
+          next(err);
+        })
+    })
+
 }

routes/auth.js
@@ -8,20 +8,36 @@ const app = express();
 app.use(express.json());
 
 app.put("/signup", [
-    body("email")
-      .isEmail()
-      .withMessage("Please enter valid email address")
-      .custom((value, {req}) => {
-        return User.findOne({where: {email: value} }).then(userDoc => {
-          if (userDoc) {
-            return Promise.reject("E-Mail address already exists!");
-          }
-        });
-      })
-      .normalizeEmail(),
-    body("password").trim().isLength({min: 8}),
-    body("name").trim().not().isEmpty()
-  ], authController.signup
+  body("email")
+    .isEmail()
+    .withMessage("Please enter valid email address")
+    .custom((value, { req }) => {
+      return User.findOne({ where: { email: value } }).then(userDoc => {
+        if (userDoc) {
+          return Promise.reject("E-Mail address already exists!");
+        }
+      });
+    })
+    .normalizeEmail(),
+  body("password").trim().isLength({ min: 8 }),
+  body("name").trim().not().isEmpty()
+], authController.signup
+);
+
+app.post("/login", [
+  body("email")
+    .isEmail()
+    .withMessage("Please enter a valid email address")
+    .custom((value, { req }) => {
+      return User.findOne({ where: { email: value } }).then(userDoc => {
+        if (!userDoc) {
+          return Promise.reject("User not found!");
+        }
+      });
+    })
+    .normalizeEmail(),
+  body("password").trim().isLength({ min: 8 }),
+], authController.login
 );
 
 module.exports = app;

commit 1057fae2ae96005b8a2c4239b3f870e742327610

fix: Implement Proper JWT with HMAC Algorithm
Fixes: 276012e83c9bcef465eef7649cf29b4c0080fb09, b2f73c4232841545fe2c650c5b3688c33196109f

controllers/auth.js
@@ -1,6 +1,7 @@
-const { validationResult } = require("express-validator/check");
+const {validationResult} = require("express-validator/check");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 exports.signup = (req, res, body) => {
@@ -25,50 +26,45 @@ exports.signup = (req, res, body) => {
       return user.save();
     })
     .then(result => {
-      res.status(201).json({ message: "User created", userId: result.id });
+      res.status(201).json({message: "User created", userId: result.id});
     }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  });
+}
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({where: {email: email}})
+    .then(user => {
+      if (!user) {
+        const error = new Error("A user with this email could not be found.")
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password)
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error("Wrong password!");
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign({
+        userId: loadedUser.id,
+        email: loadedUser.email
+      }, process.env.API_SECRET, {expiresIn: "1h"})
+      res.status(200).json({userId: loadedUser.id, token: token})
+    })
+    .catch(err => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
-      next(err);
-    });
-}
-
-exports.login = (req, res, body) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error("Login failed.");
-    error.statusCode = 422;
-    error.data = errors.array();
-    throw error;
-  }
-  const email = req.body.email;
-  const password = req.body.password;
-  User.findOne({ where: { email: email } })
-    .then(user => {
-      bcrypt
-        .compare(password, user.password)
-        .then(doMatch => {
-          if (doMatch) {
-            crypto.randomBytes(32, (err, buffer) => {
-              if (err) {
-                return res.status(500).json({ message: "Internal server error." })
-              }
-              const token = buffer.toString('hex');
-              user.loginToken = token;
-              user.loginTokenExpiration = Date.now() + 604800000 // expires in one week
-              user.save();
-              return res.status(200).json({ email: email, token: token })
-            })
-          }
-          else return res.status(422).json({ message: "Incorrect password." })
-        })
-        .catch(err => {
-          if (!err.statusCode) {
-            err.statusCode = 500;
-          }
-          next(err);
-        })
+      next(err)
     })
-
 }

models/user.js
@@ -26,15 +26,6 @@ const User = sequelize.define('user', {
       notNull: true
     }
   },
-  loginToken: {
-    type: Sequelize.STRING,
-    allowNull: true
-
-  },
-  loginTokenExpiration: {
-    type: Sequelize.DATE,
-    allowNull: true
-  },
   name: {
     type: Sequelize.STRING,
     allowNull: false,