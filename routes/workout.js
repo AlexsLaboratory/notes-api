80d1821fed05271f4f18aeb61602c46146cb3000
Alex Lowe <loweman64@outlook.com>
2021-12-09 00:36:55 -0500

    feat: Get Single Workout by ID

--- routes/workout.js
+++ routes/workout.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.post("/create", isAuth, workoutController.addWorkout);
+app.get("/get", isAuth, workoutController.getWorkout);
 app.get("/get-all", isAuth, workoutController.getWorkouts);
 
 module.exports = app;
