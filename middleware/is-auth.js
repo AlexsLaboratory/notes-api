c42f60691c35826d9e6df0760e30fd4ef7178494
Alex Lowe <loweman64@outlook.com>
2022-05-06 20:16:32 -0400

    feat: Move Features to Reusable Functions

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,25 +1,13 @@
 const jwt = require("jsonwebtoken");
+const {CustomError, catchAsync} = require("../util/error");
+const {verifyAccessToken} = require("../util/jwt");
 
-module.exports = (req, res, next) => {
+module.exports = catchAsync(async (req, res, next) => {
   const authHeader = req.get("Authorization");
-  if (!authHeader) {
-    const error = new Error("Not authenticated");
-    error.statusCode = 401;
-    throw error;
-  }
+  if (!authHeader) return next(new CustomError("Not authenticated", 401));
   const token = authHeader.split(" ")[1];
-  let decodedToken;
-  try {
-    decodedToken = jwt.verify(token, process.env.API_SECRET);
-  } catch (e) {
-    e.statusCode = 500;
-    throw e;
-  }
-  if (!decodedToken) {
-    const error = new Error("Not authenticated");
-    error.statusCode = 401;
-    throw error;
-  }
+  const decodedToken = await verifyAccessToken(token);
+  if (!decodedToken) return next(new CustomError("Not authenticated", 401));
   req.userID = decodedToken.userID
   next();
-};
+});
